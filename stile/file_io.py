bd9fa2cdcc1c45c39a2f2be7427778c7fd7bdb13
Melanie Simet <melanie.simet@gmail.com>
2014-04-25 06:27:04 -0400

    Adding file file_io.py: simple file input & output

---
+++ stile/file_io.py
@@ -0,0 +1,85 @@
+"""@file file_io.py
+Simple file input/output.
+"""
+
+try:
+    import pyfits as fits_handler
+    has_fits=True
+except:
+    try:
+        import astropy.io.fits as fits_handler
+        has_fits=True
+    except:
+        has_fits=False
+import numpy
+
+def read_fits_image(file_name,hdu=0):
+    """
+    Return the data from a single HDU extension of the FITS file file_name.  Technically this
+    doesn't have to be an image as the method is the same for table data; it's called "image" 
+    because of the default hdu=0 which can only be image data.
+    
+    Header information is not preserved, as it is not used by Stile.  Any scaling of the data
+    will be done automatically when the data is accessed.
+    
+    @param file_name A path leading to a valid FITS file
+    @param hdu       The HDU in which the requested data is located (default: 0)
+    @returns         The contents of the requested HDU
+    """
+    if has_fits:
+        fits_file = fits_handler.open(file_name)
+        data = fits_file[hdu].data
+        fits_file.close()
+        return data
+    else:
+        raise ImportError('No FITS handler found!')
+    
+def read_fits_table(file_name,hdu=1):
+    """
+    This function exists so you can read_fits_table(file_name) rather than remembering that
+    table data is usually in extension 1.
+    
+    @param file_name A path leading to a valid FITS file
+    @param hdu       The HDU in which the requested data is located (default: 1)
+    @returns         The contents of the requested HDU
+    """
+    return read_fits_image(file_name,hdu=1)
+
+def read_ascii_table(file_name, startline=None, comment=None):
+    """
+    Read in an ASCII table, and represent it via the simplest possible form for each column.  
+    
+    If you know your data contains only numbers (no strings), type safety and proper column
+    alignment can be better handled by the functions numpy.loadtxt() [complete table] or
+    numpy.genfromtxt() [missing columns].  This function is mostly useful if you have
+    string data and don't want to skip those columns or turn the file into a FITS table.
+    
+    @param file_name The name of the file containing the ASCII table
+    @param startline The number of the line to start on. startline=1 skips the first line of the
+                     file, startline=2 skips the first two lines, etc. (default: None)
+    @param comment   Ignore any lines whose first non-whitespace characters are this string.
+                     (default: None)
+    @returns         a numpy array containing the data in the file file_name
+    """
+    from stile_utils import get_vector_type
+
+    f=open(file_name,'r')
+    if startline:
+        for i in range(startline):
+            f.readline()
+    d = [line.split() for line in f.readlines()]
+    f.close()
+    if len(d[-1])==0: # In case of trailing newline
+        d = d[:-1]
+    if comment:
+        lenc = len(comment)
+        d = [tuple(dd) for dd in d if dd[0].strip()[:lenc]!=comment]
+    else:
+        d = [tuple(dd) for dd in d]
+    if len(d)==0:
+        return []
+    d_arr = numpy.array(d)
+    types = ','.join([get_vector_type(d_arr[:,i]) for i in range(len(d_arr[0]))])
+    return numpy.array(d,dtype=types)
+
+
