e383ec0255486cf0f7c6898ee1d71dd2c7fccfd9
Melanie Simet <melanie.simet@gmail.com>
2014-04-25 06:28:40 -0400

    Add __init__.py

---
+++ stile/__init__.py
@@ -0,0 +1,5 @@
+from file_io import read_fits_image, read_fits_table, read_ascii_table
+from stile_utils import parser, make_recarray
+import corr2_utils
+from corr2_utils import write_corr2_param_file, read_corr2_output_file
+
