38acb34ba5d8ef7918575655061be02f5f99663f
Rachel Mandelbaum <rmandelb@andrew.cmu.edu>
2014-06-10 14:31:44 -0400

    fill in the rest of the basic unit test for StatSysTest (#6)

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -13,9 +13,27 @@ def funcname():
     return inspect.stack()[1][3]
 
 rand_seed = 314159 # random seed for tests
-n_points_test = 1000000 # number of random points to use for unit tests of stats module
+n_points_test = 800000 # number of random points to use for unit tests of stats module
 gaussian_mean = 27.0 # Mean value for the Gaussian from which to draw random points
 gaussian_sigma = 4.0 # Sigma value for the Gaussian from which to draw random points
+
+def check_results(stats_obj):
+    """A utility to check whether a stats object contains results consistent with inputs."""
+    numpy.testing.assert_equal(n_points_test, stats_obj.N)
+    numpy.testing.assert_almost_equal(gaussian_mean/stats_obj.mean-1., 0., decimal=3,
+                                   err_msg='Unexpected result for mean of random numbers!')
+    numpy.testing.assert_almost_equal(gaussian_mean/stats_obj.median-1., 0., decimal=3,
+                                   err_msg='Unexpected result for median of random numbers!')
+    numpy.testing.assert_almost_equal(gaussian_sigma/stats_obj.stddev-1., 0., decimal=3,
+                                   err_msg='Unexpected result for mean of random numbers!')
+    numpy.testing.assert_almost_equal(gaussian_sigma**2/stats_obj.variance-1., 0., decimal=3,
+                                   err_msg='Unexpected result for mean of random numbers!')
+    for ind in range(len(stats_obj.percentiles)):
+        perc = stats_obj.percentiles[ind]
+        val = stats_obj.values[ind]
+        expected_perc = 50.*(1.0+math.erf((val-gaussian_mean)/(math.sqrt(2.)*gaussian_sigma)))
+        numpy.testing.assert_almost_equal(expected_perc/perc-1., 0., decimal=2,
+                                          err_msg='Unexpected percentile result for randoms!')
 
 def test_statsystest_basic():
     """Various tests of the basic functionality of the StatSysTest class."""
@@ -28,23 +46,15 @@ def test_statsystest_basic():
     # Run it through the StatSysTest framework and check that the outputs are as expected.
     test_obj = stile.sys_tests.StatSysTest()
     result =  test_obj(test_vec)
-    numpy.testing.assert_equal(n_points_test, result.N)
-    numpy.testing.assert_almost_equal(gaussian_mean/result.mean-1., 0., decimal=3,
-                                   err_msg='Unexpected result for mean of random numbers!')
-    numpy.testing.assert_almost_equal(gaussian_mean/result.median-1., 0., decimal=3,
-                                   err_msg='Unexpected result for median of random numbers!')
-    numpy.testing.assert_almost_equal(gaussian_sigma/result.stddev-1., 0., decimal=3,
-                                   err_msg='Unexpected result for mean of random numbers!')
-    numpy.testing.assert_almost_equal(gaussian_sigma**2/result.variance-1., 0., decimal=3,
-                                   err_msg='Unexpected result for mean of random numbers!')
-    for ind in range(len(result.percentiles)):
-        perc = result.percentiles[ind]
-        val = result.values[ind]
-        expected_perc = 50.*(1.0 + math.erf((val-gaussian_mean)/(math.sqrt(2.)*gaussian_sigma)))
-        numpy.testing.assert_almost_equal(expected_perc/perc-1., 0., decimal=2,
-                                          err_msg='Unexpected percentile result for randoms!')
+    check_results(result)
 
-    # Do the same with it as a tuple, list, and reshaped into a multi-dimensional array
+    # Do the same with it as a tuple, list, and reshaped into a multi-dimensional array.
+    result = test_obj(list(test_vec))
+    check_results(result)
+    result = test_obj(tuple(test_vec))
+    check_results(result)
+    result = test_obj(test_vec.reshape((0.5*n_points_test, 2)))
+    check_results(result)
 
     t2 = time.time()
     print 'time for %s = %.2f'%(funcname(),t2-t1)
