1e91cf7fc5ca6b04ebb92d21348921e02317321c
Melanie Simet <melanie.simet@gmail.com>
2014-07-07 02:16:50 -0400

    Adding functionality to the lsst module (#7)

--- stile/lsst/base_tasks.py
+++ stile/lsst/base_tasks.py
@@ -1,7 +1,8 @@
 import lsst.pex.config
 import lsst.pipe.base
+import lsst.meas.mosaic
 from lsst.pipe.tasks.dataIds import PerTractCcdDataIdContainer
-from .sys_tests import adapter_registry
+from .sys_test_adapters import adapter_registry
 import numpy
 
 
@@ -26,6 +27,13 @@ class CCDSingleEpochStileTask(lsst.pipe.base.CmdLineTask):
         
     def run(self,dataRef):
         catalog = dataRef.get("src",immediate=True)
+        catalog = self.removeFlags(catalog)
+        if dataRef.datasetExists("fcr_md"):
+            calib_data = dataRef.get("fcr_md")
+            calib_type="fcr"
+        else:
+            calib_data = dataRef.get("calexp_md")
+            calib_type = "calexp"
         sys_data_list = []
         extra_col_dict = {}
         for sys_test in self.sys_tests:
@@ -38,28 +46,38 @@ class CCDSingleEpochStileTask(lsst.pipe.base.CmdLineTask):
             sys_test_data.cols_list = sys_test.getRequiredColumns()
             for (mask,cols) in zip(sys_test_data.mask_list,sys_test_data.cols_list):
                 for col in cols:
-	            #print col, col in catalog.schema, catalog.schema, "check"
+                #print col, col in catalog.schema, catalog.schema, "check"
                     if not col in catalog.schema:
                         if not col in extra_col_dict:
                             extra_col_dict[col] = numpy.zeros(len(catalog))
                             extra_col_dict[col].fill('nan')
                         nan_mask = extra_col_dict[col]=='nan'
                         nan_and_col_mask = numpy.logical_and(nan_mask,mask)
-                        extra_col_dict[col][nan_and_col_mask] = self.computeExtraColumn(                                             col,catalog[nan_and_col_mask])
+                        extra_col_dict[col][nan_and_col_mask] = self.computeExtraColumn(
+                            col,catalog[nan_and_col_mask],calib_data,calib_type)
             sys_data_list.append(sys_test_data)
         
         for sys_test,sys_test_data in zip(self.sys_tests,sys_data_list):
             new_catalogs = []
             for mask,cols in zip(sys_test_data.mask_list,sys_test_data.cols_list):
-	    	new_catalog = {}
+                new_catalog = {}
                 for column in cols:
                     if column in extra_col_dict:
                         new_catalog[column] = cols[column]
                     elif column in catalog.schema:
                         new_catalog[column] = catalog[column]
                 new_catalogs.append(self.makeArray(new_catalog))
-            sys_test(*new_catalogs)
+            results = sys_test(*new_catalogs)
+            
     
+    def removeFlags(self,catalog):
+        flags = ['deblend.too-many-peaks','deblend.parent-too-big','deblend.failed',
+                 'deblend.skipped','flags.badcentroid','flags.pixel.edge','flags.pixel.bad',
+                 'flux.aperture.flags','flux.gaussian.flags','flux.kron.flags','flux.naive.flags',
+                 'flux.psf.flags']
+        mask = numpy.logical_and([catalog[flag]==False for flag in flags])
+        return catalog[mask]
+            
     def makeArray(self,catalog_dict):
         dtypes = []
         for key in catalog_dict:
@@ -69,8 +87,25 @@ class CCDSingleEpochStileTask(lsst.pipe.base.CmdLineTask):
             data[key] = catalog_dict[key]
         return data
 
-    def computeExtraColumn(self,col,data):
-        raise NotImplementedError()
+    def computeExtraColumn(self,col,data,calib_data,calib_type):
+        if col=="ra":
+            return data.getRa().asDegrees()
+        elif col=="dec":
+                return data.getDec().asDegrees()
+        elif col=="mag_err":
+            return 2.5/numpy.log(10)*(sources.getPsfFluxErr()/sources.getPsfFlux())
+        elif col=="mag":
+            if calib_type=="fcr":
+                ffp = lsst.meas.mosaic.FluxFitParams(calib_data)
+                x, y = data.getX(), data.getY()
+                correction = numpy.array(ffp.eval(x[i],y[i]) for i in range(n)])
+                zeropoint = 2.5*numpy.log10(fcr.get("FLUXMAG0")) + correction
+            elif calib_type=="calexp":
+                zeropoint = 2.5*numpy.log10(calib_data.get("FLUXMAG0"))
+            return zeropoint - 2.5*numpy.log10(data.getPsfFlux())
+        elif col=="w":
+            raise NotImplementedError("Need to figure out something clever for weights")
+        raise NotImplementedError("Cannot compute col %s"%col)
         
     @classmethod
     def _makeArgumentParser(cls):
--- stile/lsst/sys_test_adapters.py
+++ stile/lsst/sys_test_adapters.py
@@ -4,6 +4,32 @@ import numpy
 
 adapter_registry = lsst.pex.config.makeRegistry("Stile test outputs")
 
+def MaskGalaxy(data):
+    return data['classification_extendedness']==1
+
+def MaskStar(data):
+    return data['classification_extendedness']==0
+
+def MaskBrightStar(data):
+    # We could hard-code a level, or do this:
+    star_mask = mask_star(data)
+    top_tenth = numpy.percentile(data['flux.psf'][star_mask],0.9)
+    top_tenth_mask = data['flux.psf']>top_tenth
+    return numpy.logical_and(star_mask,top_tenth_mask)
+
+def MaskGalaxyLens(data):
+    # Should probably...pick the nearest/biggest/brightest ones? Randomly select?
+    return mask_galaxy(data)
+
+def MaskPSFStar(data):
+    return data['calib.psf.used']==True
+
+mask_dict = {'galaxy': MaskGalaxy,
+             'star': MaskStar,
+             'star bright': MaskBrightStar,
+             'galaxy lens': MaskGalaxyLens,
+             'star PSF': MaskPSFStar}
+
 class StarGalaxyCrossCorrelationAdapterConfig(lsst.pex.config.Config):
     pass
     
@@ -13,15 +39,16 @@ class StarGalaxyCrossCorrelationAdapter(object):
         self.config = config
         self.test = sys_tests.StarGalaxyCrossCorrelationSysTest()
         self.name = self.test.short_name
+        self.mask_funcs = [mask_dict[obj_type] for obj_type in self.test.objects_list]
      
     def __call__(self,*data):
         self.test(*data)
     
-    def getMasks(self):
-        raise NotImplementedError()
+    def getMasks(self,data):
+        return [mask_func(data) for mask_func in self.mask_funcs]
         
     def getRequiredColumns(self):
-        raise NotImplementedError()
+        return self.test.required_quantities
     
 class StatsPSFFluxAdapterConfig(lsst.pex.config.Config):
     pass
