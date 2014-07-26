d1ce47ffd0d19d32260919946168d32b45335747
Melanie Simet <melanie.simet@gmail.com>
2014-07-26 17:10:45 -0400

    Change behavior of AddCorr2Kwargs to fit with LSST config requirements (#7)
    Change AddCorr2Kwargs to only return the dict keys of interest, not to write them as a new key in the same dict, since LSST config requires all value types to be the same basic type (eg string).

--- stile/sys_tests.py
+++ stile/sys_tests.py
@@ -161,9 +161,7 @@ class CorrelationFunctionSysTest(SysTest):
 
         # First, pull out the corr2-relevant parameters from the stile_args dict, and add anything
         # passed as a kwarg to that dict.
-        if not 'corr2_kwargs' in stile_args:
-            stile_args = stile.corr2_utils.AddCorr2Dict(stile_args)
-        corr2_kwargs = copy.deepcopy(stile_args['corr2_kwargs'])
+        corr2_kwargs = stile.corr2_utils.AddCorr2Dict(stile_args)
         corr2_kwargs.update(kwargs)
         # Now, pass the data and random arguments to MakeCorr2FileKwargs.  This will write to disk
         # any data that's currently contained in memory for Stile, as well as making sure that all
