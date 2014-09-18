3792cff427678bef39745208e673d1664a6bc3a1
Melanie Simet <melanie.simet@gmail.com>
2014-09-18 13:43:14 -0400

    Re-add chip and sky level sigma types (#18)

--- stile/hsc/base_tasks.py
+++ stile/hsc/base_tasks.py
@@ -153,7 +153,8 @@ class CCDSingleEpochStileTask(lsst.pipe.base.CmdLineTask):
                 if any([key in cols_list for key in
                                 ['g1_sky', 'g1_err_sky', 'g2_sky', 'g2_err_sky',
                                  'g1_chip', 'g1_err_chip', 'g2_chip', 'g2_err_chip',
-                                 'sigma', 'sigma_err', 'w']]):
+                                 'sigma_sky', 'sigma_chip', 'sigma_err_sky', 'sigma_err_chip', 
+                                 'w']]):
                     shape_masks.append(self._computeShapeMask(catalog))
                 else:
                     shape_masks.append(True)
@@ -256,12 +257,13 @@ class CCDSingleEpochStileTask(lsst.pipe.base.CmdLineTask):
         # are any moments-related keys to pull out of the catalog.
         base_shape_keys = ['g1', 'g2', 'psf_g1', 'psf_g2', 'w', 'g1_err', 'g2_err', 'psf_g1_err',
                            'psf_g2_err', 'sigma', 'sigma_err', 'psf_sigma', 'psf_sigma_err']
-        shape_keys = ['g1_sky', 'g1_chip', 'g2_sky', 'g2_chip', 'sigma']
+        shape_keys = ['g1_sky', 'g1_chip', 'g2_sky', 'g2_chip', 'sigma_sky', 'sigma_chip']
         shape_err_keys = ['g1_err_sky', 'g1_err_chip', 'g2_err_sky', 'g2_err_chip', 
-                          'sigma_err', 'w']
-        psf_shape_keys = ['psf_g1_sky', 'psf_g1_chip', 'psf_g2_sky', 'psf_g2_chip', 'psf_sigma']
+                          'sigma_err_sky', 'sigma_err_chip', 'w']
+        psf_shape_keys = ['psf_g1_sky', 'psf_g1_chip', 'psf_g2_sky', 'psf_g2_chip', 
+                          'psf_sigma_sky', 'psf_sigma_chip']
         psf_shape_err_keys = ['psf_g1_err_sky', 'psf_g1_err_chip', 'psf_g2_err_sky',
-                              'psf_g2_err_chip', 'psf_sigma_err']
+                              'psf_g2_err_chip', 'psf_sigma_err_sky', 'psf_sigma_err_chip']
         do_shape = [col for col in shape_keys if col in cols and not col in catalog.schema]
         do_err = [col for col in shape_err_keys if col in cols and not col in catalog.schema]
         do_psf = [col for col in psf_shape_keys if col in cols and not col in catalog.schema]
@@ -504,10 +506,13 @@ class CCDSingleEpochStileTask(lsst.pipe.base.CmdLineTask):
             extra_mask = None
         fake_g1 = None if g1 is None else numpy.zeros(len(g1))
         fake_g2 = None if g2 is None else numpy.zeros(len(g2))
+        fake_sigma = None if sigma is None else numpy.zeros(len(sigma))
         fake_g1_err = None if g1_err is None else numpy.zeros(len(g1_err))
         fake_g2_err = None if g2_err is None else numpy.zeros(len(g2_err))
+        fake_sigma_err = None if sigma_err is None else numpy.zeros(len(sigma_err))
         fake_psf_g1 = None if psf_g1 is None else numpy.zeros(len(psf_g1))
         fake_psf_g2 = None if psf_g2 is None else numpy.zeros(len(psf_g2))
+        fake_psf_sigma = None if psf_sigma is None else numpy.zeros(len(psf_sigma))
         
         if sky_coords:
             # convert degree to arcsec
@@ -515,10 +520,11 @@ class CCDSingleEpochStileTask(lsst.pipe.base.CmdLineTask):
             if sigma_err is not None: sigma_err *= 3600.
             if psf_sigma is not None: psf_sigma *= 3600.
             return ({'g1': fake_g1, 'g2': fake_g2, 'g1_err': fake_g1_err, 'g2_err': fake_g2_err, 
-                     'psf_g1': fake_psf_g1, 'psf_g2': fake_psf_g2,
-                     'g1_sky': g1, 'g2_sky': g2, 'sigma': sigma, 'g1_err_sky': g1_err, 
-                     'g2_err_sky': g2_err, 'sigma_err': sigma_err, 'w': w, 
-                     'psf_g1_sky': psf_g1, 'psf_g2_sky': psf_g2, 'psf_sigma': psf_sigma},
+                     'sigma': fake_sigma, 'psf_g1': fake_psf_g1, 'psf_g2': fake_psf_g2, 
+                     'psf_sigma': fake_psf_sigma,
+                     'g1_sky': g1, 'g2_sky': g2, 'sigma_sky': sigma, 'g1_err_sky': g1_err, 
+                     'g2_err_sky': g2_err, 'sigma_err_sky': sigma_err, 'w': w, 
+                     'psf_g1_sky': psf_g1, 'psf_g2_sky': psf_g2, 'psf_sigma_sky': psf_sigma},
                      extra_mask)
         else:
             # convert pixel to arcsec
@@ -526,10 +532,11 @@ class CCDSingleEpochStileTask(lsst.pipe.base.CmdLineTask):
             if sigma_err is not None: sigma_err *= 0.167
             if psf_sigma is not None: psf_sigma *= 0.167
             return ({'g1': fake_g1, 'g2': fake_g2, 'g1_err': fake_g1_err, 'g2_err': fake_g2_err, 
-                     'psf_g1': fake_psf_g1, 'psf_g2': fake_psf_g2,
-                     'g1_chip': g1, 'g2_chip': g2, 'sigma': sigma, 'g1_err_chip': g1_err, 
-                     'g2_err_chip': g2_err, 'sigma_err': sigma_err, 'w': w, 
-                     'psf_g1_chip': psf_g1, 'psf_g2_chip': psf_g2, 'psf_sigma': psf_sigma},
+                     'sigma': fake_sigma, 'psf_g1': fake_psf_g1, 'psf_g2': fake_psf_g2, 
+                     'psf_sigma': fake_psf_sigma,
+                     'g1_chip': g1, 'g2_chip': g2, 'sigma_chip': sigma, 'g1_err_chip': g1_err, 
+                     'g2_err_chip': g2_err, 'sigma_err_chip': sigma_err, 'w': w, 
+                     'psf_g1_chip': psf_g1, 'psf_g2_chip': psf_g2, 'psf_sigma_chip': psf_sigma},
                      extra_mask)
 
     def computeExtraColumn(self, col, data, calib_data, calib_type, xy0=None):
