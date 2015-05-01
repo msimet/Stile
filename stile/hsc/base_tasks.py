a52620b6883dbde210d4552e8760b5c29f1a983d
Hironao Miyatake <miyatake@astro.princeton.edu>
2015-05-01 14:21:00 -0400

    Added 'flags.pixel.clipped.any' to tract level configs (#43)

--- stile/hsc/base_tasks.py
+++ stile/hsc/base_tasks.py
@@ -892,7 +892,8 @@ class PatchSingleEpochStileConfig(CCDSingleEpochStileConfig):
                    'centroid.naive.flags', 'flags.pixel.edge', 'flags.pixel.interpolated.any',
                    'flags.pixel.interpolated.center', 'flags.pixel.saturated.any',
                    'flags.pixel.saturated.center', 'flags.pixel.cr.any', 'flags.pixel.cr.center',
-                   'flags.pixel.bad', 'flags.pixel.suspect.any', 'flags.pixel.suspect.center'])
+                   'flags.pixel.bad', 'flags.pixel.suspect.any', 'flags.pixel.suspect.center',
+                   'flags.pixel.clipped.any'])
 
 
 class PatchSingleEpochStileTask(CCDSingleEpochStileTask):
@@ -979,7 +980,8 @@ class TractSingleEpochStileConfig(CCDSingleEpochStileConfig):
                    'centroid.naive.flags', 'flags.pixel.edge', 'flags.pixel.interpolated.any',
                    'flags.pixel.interpolated.center', 'flags.pixel.saturated.any',
                    'flags.pixel.saturated.center', 'flags.pixel.cr.any', 'flags.pixel.cr.center',
-                   'flags.pixel.bad', 'flags.pixel.suspect.any', 'flags.pixel.suspect.center'])
+                   'flags.pixel.bad', 'flags.pixel.suspect.any', 'flags.pixel.suspect.center',
+                   'flags.pixel.clipped.any'])
 
     ccd_type = 'S7' # NumPy string dtype, 7 characters long 
 
