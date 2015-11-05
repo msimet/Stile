ffed98d0d8c6fa67e5ec450bcee55c1c6f9ce0d7
Melanie Simet <melanie.simet@gmail.com>
2015-11-05 01:45:43 -0500

    Add check for treecorr import in setup.py and sys_tests.py (#57)

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+try:
+    import treecorr
+except ImportError:
+    import warnings
+    warnings.warn("treecorr package cannot be imported. Installation will proceed, but you may "+
+                  "wish to install it if you would like to use the correlation functions within "+
+                  "Stile.")
+
 
 setup(name='Stile',
       version='0.1',
       description='Stile: Systematics Tests in Lensing pipeline',
       author='The Stile team',
+      requirements=['numpy'],
       author_email='melanie.simet@gmail.com',
       url='https://github.com/msimet/Stile',
       packages=['stile', 'stile.hsc'],
