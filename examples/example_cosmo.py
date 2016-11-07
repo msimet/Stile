cce156dba578112682596a93350bfc99ca73afda
Melanie Simet <melanie.simet@gmail.com>
2016-11-07 13:02:03 -0500

    Remove some print statements; get working with destest (#90)

--- examples/example_cosmo.py
+++ examples/example_cosmo.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('..')
 import stile
 import stile.cosmo_forecasts
-import destest.src.cosmo
+import destest.src.cosmo as cosmo
 import numpy
 
 def main():
@@ -15,7 +15,7 @@ def main():
     if True:
         # Run on a subset of CFHTLenS data
         data = stile.ReadTable(#'/homes/m/msimet/CFHTLenS/CFHTLens_W1_passed.fits', 
-                               '/physics2/msimet/lustre/cfhtlens/CFHTLens_W1_passed.fits',
+                               '/physics2/msimet/lustre/cfhtlens/CFHTLens_W2_reject0.fits',
                                fields={'psf_g1': 'PSF_e1', 'psf_g2': 'PSF_e2', 'w': 'weight', 'g1': 'e1', 'g2': 'e2',
                                        'sigma': 'scalelength', 'psf_sigma': 'FLUX_RADIUS'})
         data = numpy.array(data)[:1000]
@@ -31,9 +31,10 @@ def main():
                   'min_sep': 0.1, 'max_sep': 500, 'sep_units': 'arcmin', 'nbins': 25}
     list_of_results = [test(data, config=config) for test in list_of_tests]
     
-    cosmo = stile.cosmo_forecasts.XiSet(data, data, config, list_of_results)
-    deltaxi = cosmo.computeError()
-    print res 
+    
+    cosmo_obj = stile.cosmo_forecasts.XiSet(data, data, config, list_of_results)
+    deltaxi = cosmo_obj.computeError()
+    print deltaxi
 
     # Prep destest
     workdir='../destest'
