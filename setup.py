805b8220acdf502275e543e3ac182918a5ccbf53
Francois <fr.eiffel@gmail.com>
2018-07-11 18:29:09 -0400

    Updates to python3 and updates setup script

--- setup.py
+++ setup.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
-try:
-    import treecorr
-except ImportError:
-    import warnings
-    warnings.warn("treecorr package cannot be imported. Installation will proceed, but you may "+
-                  "wish to install it if you would like to use the correlation functions within "+
-                  "Stile.")
-
+# read the contents of the README file
+with open('README.md', encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(name='Stile',
       version='0.1',
       description='Stile: Systematics Tests in Lensing pipeline',
       author='The Stile team',
-      requirements=['numpy'],
+      install_requires=['numpy', 'treecorr', 'astropy', 'matplotlib'],
       author_email='melanie.simet@gmail.com',
       url='https://github.com/msimet/Stile',
       packages=['stile', 'stile.hsc'],
       scripts=['bin/StileVisit.py', 'bin/StileVisitNoTract.py', 'bin/StileCCD.py',
-               'bin/StileCCDNoTract.py', 'bin/StilePatch.py', 'bin/StileTract.py']
+               'bin/StileCCDNoTract.py', 'bin/StilePatch.py', 'bin/StileTract.py'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Scientific/Engineering :: Physics'
+      ]
       )
